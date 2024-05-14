56e0d09a0b7353bbef3a658d46c280eccf37bcad
nmorduch <1051450+nmorduch@users.noreply.github.com>
2024-05-14 21:23:42 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './index.scss';
 
 const settings = {
   'viz__id': (el) => {
+    let a = el.querySelector('.dataviz__title-container')
+    let b = el.querySelector('.dataviz__chart-container')
+    a && a.remove()
+    b && b.remove()
+
+    let body = document.getElementById("na-report")
     let target = document.createElement("div")
     target.classList.add("iframe-wrapper")
     el.appendChild(target)
@@ -11,7 +17,7 @@ const settings = {
     target.appendChild(iframe)
 
     const scrollTargetIntoView = () => {
-      target.scrollIntoView({block: "start", inline: "nearest", behavior: "smooth"})
+      target.scrollIntoView({ block: "center", inline: "center", behavior: "smooth" })
     }
     
     // Pulled from https://pqina.nl/blog/applying-styles-based-on-the-user-scroll-position-with-smart-css/
@@ -39,6 +45,7 @@ const settings = {
       // console.log(target.getBoundingClientRect().y)
       if((target.getBoundingClientRect().y < 100) && (target.getBoundingClientRect().y >= -1)) {
         target.classList.remove("out-of-frame");
+        body.classList.remove("scroll-reverse")
       } else {
         target.classList.add("out-of-frame");
       }
